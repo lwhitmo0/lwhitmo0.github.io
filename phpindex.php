009b3c5adf4d2520a0dcff76c9e9d6f8eb5d5e99
lwhitmo0 <148815855+lwhitmo0@users.noreply.github.com>
2024-10-02 10:21:42 -0400

    Create phpindex.php

---
+++ phpindex.php
@@ -0,0 +1,8 @@
+○	$sOutputString = "<hr/>";
+○	
+○	for($iCounter = 1; $iCounter <= 1000; $iCounter++)
+○	{
+○	     $sOutputString .= $iCounter . ") Whitmore got PHP running on the computer!! ";
+○	}
+○	
+○	echo ("<h5>" . $sOutputString . "</h5><hr/>");
